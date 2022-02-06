2001ed685b0ae3e291eba1b574cfbc5d58c5f3c6
Alexander Alexeev <alekseev@trueconf.ru>
2022-02-06 17:57:07 +0300

    update mixins

--- src/theme/styleMixins.ts
+++ src/theme/styleMixins.ts
@@ -1,3 +1,5 @@
+import {hexToRgb} from '../utils';
+
 interface ISizeMixin {
   width: number | string,
   height: number | string,
@@ -8,13 +10,40 @@ export const sizeMixin = (value: number | string): ISizeMixin => ({
   height: value,
 });
 
-export const betweenChildrenMixin = (value: any) => ({
+type stylesContentValueInputType = { [key: string]: string | number };
+
+interface IBetweenChildrenMixinOutput {
+  [key: string]: {
+    [key: string]: stylesContentValueInputType,
+  },
+}
+
+export const betweenChildrenMixin = (value: stylesContentValueInputType): IBetweenChildrenMixinOutput => ({
   '& > *': {
     '&:not(:last-child)': {
       ...value,
     },
   },
 });
+
+interface IBreakpointMixinOutput {
+  [key: string]: stylesContentValueInputType,
+}
+
+export const breakpointMixin = (min: number, max: number, content: any): IBreakpointMixinOutput => {
+  if (max === 0) {
+    return {
+      [`@media only screen and (min-width: ${min + 1}px)`]: {
+        ...content,
+      },
+    };
+  }
+  return {
+    [`@media only screen and (min-width: ${min}px) and (max-width: ${max - 1}px)`]: {
+      ...content,
+    },
+  };
+};
 
 interface IFlexLayoutMixin {
   display: string,
@@ -27,3 +56,38 @@ export const flexLayoutMixin = (alignItems = 'center', justifyContent = 'center'
   alignItems,
   justifyContent,
 });
+
+export const oneLineTextMixin = () => ({
+  overflow: 'hidden',
+  whiteSpace: 'nowrap',
+  textOverflow: 'ellipsis',
+  width: '100%',
+});
+
+export const customScrollBarMixin = (hex: string) => {
+  const {r, g, b} = hexToRgb(hex);
+
+  return {
+    overflow: 'auto',
+    '&::-webkit-scrollbar': {
+      width: 8,
+      background: `rgba(${r}, ${g}, ${b}, 0.15)`,
+      margin: 0,
+    },
+    '&::-webkit-scrollbar-button:vertical:decrement': {
+      backgroundColor: 'rgba(0, 0, 0, 0.0)',
+      height: 8,
+    },
+    '&::-webkit-scrollbar-button:vertical:increment': {
+      backgroundColor: 'rgba(0, 0, 0, 0.0)',
+      height: 8,
+    },
+    '&::-webkit-scrollbar-thumb': {
+      minHeight: 32,
+      background: '#999',
+      borderRadius: 2,
+      backgroundClip: 'content-box',
+    },
+    '-webkit-overflow-scrolling': 'touch',
+  };
+};
