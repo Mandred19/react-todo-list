1bfd47bdd335b46b945dbba12a78fac576f44e4c
Alexander Alexeev <alekseev@trueconf.ru>
2022-02-06 18:16:48 +0300

    update react scripts

--- src/screens/AppContent/AppContentTable.tsx
+++ src/screens/AppContent/AppContentTable.tsx
@@ -44,7 +44,7 @@ function createData(
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Cupcake111', 305, 3.7, 67, 4.3),
   createData('Donut', 452, 25.0, 51, 4.9),
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
